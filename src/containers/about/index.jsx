be6a603008af24b7a036c2cda00d70cc30930f70
Avogardo <avogardo0@gmail.com>
2018-03-16 20:10:15 +0100

    create environment

---
+++ src/containers/about/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    <h1>About Us</h1>
+    <p>
+      What about us?<br/>
+      What about all the times you said you had the answers?<br/>
+      What about us?<br/>
+      What about all the broken happy ever afters?
+    </p>
+  </div>
+);
