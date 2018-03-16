a3a417f6b1e2160bcc135a21475f58c20b63982f
Avogardo <avogardo0@gmail.com>
2018-03-16 20:31:34 +0100

    show count number to about component

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 
-export default () => (
+
+const About = props => (
   <div>
     <h1>About Us</h1>
     <p>
+      Counter: {props.count}<br/>
       What about us?<br/>
       What about all the times you said you had the answers?<br/>
       What about us?<br/>
@@ -11,3 +14,12 @@ export default () => (
     </p>
   </div>
 );
+
+const mapStateToProps = state => ({
+  count: state.counter.count,
+});
+
+export default connect(
+  mapStateToProps,
+  null,
+)(About);
