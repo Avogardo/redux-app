e8f87aededf66d98c3009f51047f508bcb2ff0f8
Avogardo <avogardo0@gmail.com>
2018-03-21 21:08:37 +0100

    Home is pure function component again

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -11,85 +11,62 @@ import { getListAsync, getDataRequested } from '../../modules/api/actions';
 import { goToAbout } from '../../modules/routerActions';
 
 
+const Home = props => (
+  <div>
+    <h1>Home</h1>
+    <p>Count: {props.count}</p>
+    <p>Data: {props.list.a}</p>
 
-class Home extends React.Component {
-  render() {
-    const {
-      count,
-      list,
-      increment,
-      isIncrementing,
-      incrementAsync,
-      decrement,
-      isDecrementing,
-      decrementAsync,
-      changePage,
-      getListAsync,
-      isLoadingTitle,
-      isLoading,
-      isError,
-      repositories,
-      getDataRequested,
-    } = this.props;
+    <p>
+      <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
+      <button
+        onClick={props.incrementAsync}
+        disabled={props.isIncrementing}
+        style={{cursor: props.isIncrementing ? 'wait' : 'default'}}
+      >
+        Increment Async
+      </button>
+    </p>
 
-    return (
-      <div>
-        <h1>Home</h1>
-        <p>Count: {count}</p>
-        <p>Data: {list.a}</p>
+    <p>
+      <button onClick={props.decrement} disabled={props.isDecrementing}>Decrementing</button>
+      <button
+        onClick={props.decrementAsync}
+        disabled={props.isDecrementing}
+        style={{cursor: props.isDecrementing ? 'wait' : 'default'}}
+      >
+        Decrement Async
+      </button>
+    </p>
 
-        <p>
-          <button onClick={increment} disabled={isIncrementing}>Increment</button>
-          <button
-            onClick={incrementAsync}
-            disabled={isIncrementing}
-            style={{cursor: isIncrementing ? 'wait' : 'default'}}
-          >
-            Increment Async
-          </button>
-        </p>
+    <p>
+      <button onClick={() => props.changePage()}>
+        Go to about page via redux
+      </button>
+    </p>
 
-        <p>
-          <button onClick={decrement} disabled={isDecrementing}>Decrementing</button>
-          <button
-            onClick={decrementAsync}
-            disabled={isDecrementing}
-            style={{cursor: isDecrementing ? 'wait' : 'default'}}
-          >
-            Decrement Async
-          </button>
-        </p>
+    <p>
+      <button
+        onClick={() => {props.getListAsync(); props.getDataRequested()}}
+        disabled={props.isLoadingTitle}
+        style={{cursor: props.isLoadingTitle ? 'wait' : 'default'}}
+      >
+        Get data
+      </button>
+    </p>
 
-        <p>
-          <button onClick={() => changePage()}>
-            Go to about page via redux
-          </button>
-        </p>
-
-        <p>
-          <button
-            onClick={() => {getListAsync(); getDataRequested() }}
-            disabled={isLoadingTitle}
-            style={{cursor: isLoadingTitle ? 'wait' : 'default'}}
-          >
-            Get data
-          </button>
-        </p>
-
-        <div>
-          {!isLoading && !isError ? repositories.map((item, index) =>
-            <div key={index}>
-              {item.name}
-            </div>
-          )
-          :
-          <span>loading...</span>
-        }
+    <div>
+      {!props.isLoading && !props.isError ? props.repositories.map((item, index) =>
+        <div key={index}>
+          {item.name}
         </div>
-      </div>
-    );
-  }
-};
+      )
+      :
+      <span>loading...</span>
+    }
+    </div>
+  </div>
+);
 
 const mapStateToProps = state => ({
   count: state.counter.count,
